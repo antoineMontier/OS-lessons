fb163880a90249291412e723086c5cf88a76b429
Antoine <antoine.montier@sfr.fr>
2023-01-26 12:03:13 +0100

    improved my recursive function

--- processes/binary_fork.c
+++ processes/binary_fork.c
@@ -3,30 +3,25 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-void binary_forking(int nb);
+void binary_forking_rec(int nb);
 
 int main(){//gcc -c binary_fork.c && gcc -o binary_fork binary_fork.o && ./binary_fork
 
 
-    binary_forking(3);
+    binary_forking_rec(3);
     return 0;
 }
 
-void binary_forking(int nb){
+void binary_forking_rec(int nb){
     if(nb <= 0) return;
-    pid_t son1_pid = fork();
-    pid_t son2_pid;
-    
-    if(son1_pid > 0){//father
-        son2_pid = fork();
-        if(son2_pid > 0){//father
-            printf("I'm the father %d, son1 %d,     son2 %d\n", getpid(), son1_pid, son2_pid);
-            wait(NULL);
-            wait(NULL);
-            exit(0);
-        }
+    pid_t son1_pid, son2_pid;
+
+    if(      (son1_pid = fork())     &&     (son2_pid = fork())    ){//father
+        printf("I'm the father %d, son1 %d,     son2 %d\n", getpid(), son1_pid, son2_pid);
+        wait(NULL);
+        wait(NULL);
+        exit(0);
     }
 
-    binary_forking(nb-1);
-
+    binary_forking_rec(nb-1);
 }
