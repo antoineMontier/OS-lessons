c42868c1c3211f9a7dc498d2a03b72d30535a4ef
Antoine <antoine.montier@sfr.fr>
2023-01-26 10:45:43 +0100

    added linear fork topology

---
+++ processes/linear_fork.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+void forking(int nb);
+
+int main(){
+
+    forking(5);
+
+    sleep(4);
+    return 0;
+}
+
+void forking(int nb){
+    if(nb <= 0) return;
+    pid_t son_pid = fork();
+    if(son_pid == 0){
+        printf("I'm the child of %d ; my pid is %d\n", getppid(), getpid());
+        forking(nb-1);
+        wait(NULL);
+        exit(0);
+    }
+}
