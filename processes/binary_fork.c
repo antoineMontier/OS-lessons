fc0a9231f4a93b9cb657667a64e270ba52f7f50c
Antoine <antoine.montier@sfr.fr>
2023-02-01 10:45:14 +0100

    added com

--- processes/binary_fork.c
+++ processes/binary_fork.c
@@ -27,16 +27,14 @@ void binary_forking_it(int nb){
 }
 
 
-void binary_forking_rec(int nb){
+void binary_forking_rec(int nb){ // the number of processes will be 2^{nb+1} - 1
     if(nb <= 0) return;
     pid_t son1_pid, son2_pid;
-
     if(      (son1_pid = fork())     &&     (son2_pid = fork())    ){//father
         printf("I'm the father %d, son1 %d,     son2 %d\n", getpid(), son1_pid, son2_pid);
         wait(NULL);
         wait(NULL);
         exit(0);
     }
-
     binary_forking_rec(nb-1);
 }
