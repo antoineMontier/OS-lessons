0f022fd9ae1f4e0d93ec12e31f2248ecf398272f
Antoine <antoine.montier@sfr.fr>
2023-02-02 11:14:56 +0100

    cleaned the code

--- processes/parentvoyelle.c
+++ processes/parentvoyelle.c
@@ -5,17 +5,14 @@
 #include <ctype.h>
 
 int main(int argc, char **argv){
+    int n;
+
     if(argc == 1)
         return 0;
     for(int i=1; i<argc; i++){
         if(fork() == 0){
             execl("/home/antoine/Prog/c/Cours/processes/mainvoyelle", "/home/antoine/Prog/c/Cours/processes/mainvoyelle", argv[i], NULL);
-            int n=-1;
-            wait(&n);
-            n = WEXITSTATUS(n);
-            exit(n);
         }else{
-            int n;
             wait(&n);
             n = WEXITSTATUS(n);
             printf("In %s file, the most used voeylle is : %c\n", argv[i], n);
