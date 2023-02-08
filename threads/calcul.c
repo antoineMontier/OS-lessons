4d45d6b0d785b5066b7b6eb6211fe9aaa4652ef9
Antoine <antoine.montier@sfr.fr>
2023-02-08 10:48:02 +0100

    working on parralelisation of the calcul function with multiple threads

--- threads/calcul.c
+++ threads/calcul.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-void calculate(){
+void* calculate(void*);
+
+void* calculate(void*){
     int k;
     do{k = rand();}while(k != RAND_MAX-1);
+    pthread_exit(NULL);
+    /*
+    One call : 
+
+    real	0m8,589s // time between the start and end
+    user	0m8,580s // time took by the process to execute
+    sys	    0m0,008s // time took by the system access functions (fork, FILE*..)
+
+
+    Two calls :
+
+    real	0m14,546s
+    user	0m14,544s
+    sys	    0m0,001s
+    */
 }
 
 int main(){
-    calculate();
+    pthread_t my_thread;
+    int a = pthread_create(&my_thread, NULL, &calculate, NULL);
+    if(a == 0)
+        printf("sucess\n");
+    else if(a > 0)
+        printf("error with code: %d\n", a);
+
+
+    
+    calculate(NULL);
     return 0;
 }
