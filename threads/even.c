c1d13d79485fabb7334edae4dca5c32c78237179
Antoine <antoine.montier@sfr.fr>
2023-02-08 12:11:37 +0100

    now the numbers are well displayed odd-even-odd-even...

--- threads/even.c
+++ threads/even.c
@@ -3,28 +3,40 @@
 #include <pthread.h>
 #include <unistd.h>
 
+int i;
+
+
 void even(){
-    for(int i = 0; i < 1000 ; i++){
-        if(i % 2 == 0)
+    int last;
+    while(i < 1000){
+        if(last != i && !(i % 2)){
             printf("even %d\n", i);
-        usleep(1);
-    }
+            last = i;
+        }
+    }    
+    pthread_exit(NULL);
 }
 
 void odd(){
-    for(int i = 0; i < 1000 ; i++){
-        if(i % 2 == 1)
+    int last;
+    while(i < 1000){
+        if(last != i && (i % 2)){
             printf("odd %d\n", i);
-        usleep(1);
+            last = i;
+        }
     }
+    pthread_exit(NULL);
 }
-
 
 int main(){ // compile with gcc -pthread even.c -o even
     pthread_t even_t, odd_t;
 
     pthread_create(&even_t, NULL, (void*)even, NULL);
     pthread_create(&odd_t, NULL, (void*)odd, NULL);
+
+
+    for(i = 0 ; i < 1000 ; i++){usleep(1);}
+
 
     pthread_join(even_t, NULL);
     pthread_join(odd_t, NULL);
