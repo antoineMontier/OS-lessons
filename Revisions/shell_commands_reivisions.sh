bc52fb45af17b511e7fc60ec5f129f3a031528cc
Antoine <antoine.montier@sfr.fr>
2023-05-10 15:45:56 +0200

    worked on bash => fibonacci

--- Revisions/shell_commands_reivisions.sh
+++ Revisions/shell_commands_reivisions.sh
@@ -8,6 +8,25 @@ function count_files {
     echo "file number $nb"
 }
 
+function fibo {
+    if(($1<=1)) ; then 
+        echo $1 
+    else 
+        one=$(expr $1 - 1)
+        fibo_one=$(fibo $one)
+        two=$(expr $1 - 2)
+        fibo_two=$(fibo $two)
+        calc=$(expr $fibo_one + $fibo_two)
+        echo $calc
+    fi
+}
 
+function fibo_string {
+    nb=$(fibo $1)
+    echo "fibo($1) = $nb"
+}
 
-count_files
+for ((i=0 ; i<20 ; i++))
+do
+    fibo_string $i
+done
